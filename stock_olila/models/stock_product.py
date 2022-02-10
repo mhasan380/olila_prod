42cb8dc93cac0897770f891fde975babc845838c
Mahmudul Hasan <mhasan380@gmail.com>
2022-02-10 11:46:43 +0600

    ADD: Goods type, filter net stock

--- stock_olila/models/stock_product.py
+++ stock_olila/models/stock_product.py
@@ -1,10 +1,24 @@
-from odoo import api, fields, models
+import operator as py_operator
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+OPERATORS = {
+    '<': py_operator.lt,
+    '>': py_operator.gt,
+    '<=': py_operator.le,
+    '>=': py_operator.ge,
+    '=': py_operator.eq,
+    '!=': py_operator.ne
+}
 
 class ProductTemplate(models.Model):
     _inherit = 'product.product'
 
-    net_stock = fields.Float(compute="_compute_net_qty",string='Net Stock')
-    sales_price_total = fields.Float(compute="_compute_sale_price_total", string='Total Sales Price')
+    net_stock = fields.Float(compute="_compute_net_qty",string='Net Stock', search='_search_net_stock' )
+    sales_price_total = fields.Float(compute="_compute_sale_price_total", string='Total Sales Price',search='_search_sales_price_total')
+    goods_type = fields.Selection([('finish', 'Finished Goods'), ('spare', 'Spare Parts'), ('raw', 'Raw Materials')],string='Goods Type')
+
 
     @api.depends('qty_available','outgoing_qty')
     def _compute_net_qty(self):
@@ -15,6 +29,32 @@ class ProductTemplate(models.Model):
     def _compute_sale_price_total(self):
         for product in self:
             product.sales_price_total = product.qty_available * product.lst_price
+
+    def _search_net_stock(self, operator, value):
+        # TDE FIXME: should probably clean the search methods
+        return self._search_product_quantity(operator, value, 'net_stock')
+    def _search_sales_price_total(self, operator, value):
+        # TDE FIXME: should probably clean the search methods
+        return self._search_product_quantity(operator, value, 'sales_price_total')
+
+    def _search_product_quantity(self, operator, value, field):
+        # TDE FIXME: should probably clean the search methods
+        # to prevent sql injections
+        if field not in ('net_stock','sales_price_total'):
+            raise UserError(_('Invalid domain left operand %s', field))
+        if operator not in ('<', '>', '=', '!=', '<=', '>='):
+            raise UserError(_('Invalid domain operator %s', operator))
+        if not isinstance(value, (float, int)):
+            raise UserError(_('Invalid domain right operand %s', value))
+
+        # TODO: Still optimization possible when searching virtual quantities
+        ids = []
+        # Order the search on `id` to prevent the default order on the product name which slows
+        # down the search because of the join on the translation table to get the translated names.
+        for product in self.with_context(prefetch_fields=False).search([], order='id'):
+            if OPERATORS[operator](product[field], value):
+                ids.append(product.id)
+        return [('id', 'in', ids)]
 
 
     @api.model
