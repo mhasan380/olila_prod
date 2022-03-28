8a661dd06e12e032328ebdbb34f0bae189b119eb
Mahmudul Hasan <mhasan380@gmail.com>
2022-03-28 11:30:16 +0600

    ADD: Department Added in Purchase Request Line

--- olila_requisition_report/models/purchase_request.py
+++ olila_requisition_report/models/purchase_request.py
@@ -4,3 +4,9 @@ class PurchaseRequest(models.Model):
     _inherit = "purchase.request"
 
     requisition_month = fields.Date('Requisition Month')
+
+
+class PurchaseRequestLine(models.Model):
+    _inherit = "purchase.request.line"
+
+    department = fields.Many2one('hr.department', string='Department')
