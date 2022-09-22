ea9a59c08cbb4863d7b0d5ce0b857e0eaa4dd55e
Mahmudul Hasan <mhasan380@gmail.com>
2022-09-22 10:25:13 +0600

    ADD: Invoice Status Change

--- olila_sale_extend/models/sale_order.py
+++ olila_sale_extend/models/sale_order.py
@@ -6,3 +6,7 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     sales_ref_id = fields.Many2one('hr.employee',string="Sales Reference")
+
+    def change_invoice_state(self):
+        self.invoice_status = 'invoiced'
+
