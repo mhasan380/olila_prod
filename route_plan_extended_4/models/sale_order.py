64ed49a00544c0cf762dbb39c0bfa1f4770b4907
Mahmudul Hasan <mhasan380@gmail.com>
2023-01-21 11:23:58 +0600

    FIX: Route Plan Bug

--- route_plan_extended_4/models/sale_order.py
+++ route_plan_extended_4/models/sale_order.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api, _
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    division_id = fields.Many2one('route.division', string="Division", related='partner_id.division_id')
-    district_id = fields.Many2one('route.district', string="Distict", related='partner_id.district_id')
-    upazila_id = fields.Many2one('route.upazila', string="Upazila", related='partner_id.upazila_id')
-    union_id = fields.Many2one('route.union', string="Union", related='partner_id.union_id')
+    division_id = fields.Many2one('route.division', string="Division", related='partner_id.division_id', store= True)
+    district_id = fields.Many2one('route.district', string="Distict", related='partner_id.district_id', store= True)
+    upazila_id = fields.Many2one('route.upazila', string="Upazila", related='partner_id.upazila_id', store= True)
+    union_id = fields.Many2one('route.union', string="Union", related='partner_id.union_id', store= True)
