482eda081fdacf3a8d491c01bf0c6d0c351ab421
Mahmudul Hasan <mhasan380@gmail.com>
2023-04-10 10:31:16 +0600

    FIX: Customer Assign in routes

--- route_plan_extended_3/models/routes.py
+++ route_plan_extended_3/models/routes.py
@@ -5,11 +5,18 @@ class RouteMaster(models.Model):
 
     def assign_customers(self):
         if self.route_type == 'secondary':
+            self.secondary_customer_ids = [(6, 0, [])]
             customers = []
             customer_list = self.env['customer.secondary'].search([('route_id', '=', self.id)])
             for customer in customer_list:
-                if customer.id not in customers:
-                    customers.append(customer.id)
-            self.update({
-                'secondary_customer_ids': [(6, 0, customers)],
-            })
+                prod = (0, 0, {'customer_id': customer.id,
+                               'customer_code': customer.outlet_code,
+                               })
+                customers.append(prod)
+        self.write({'secondary_customer_ids': customers})
+
+
+    def auto_assign(self):
+        routes = self.env['route.master'].search([])
+        for rec in routes:
+            rec.assign_customers()
